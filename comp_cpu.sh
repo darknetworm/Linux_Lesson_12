04ef5a830432c07e7ca703c9de741620933f0dba
Paul <82410807+darknetworm@users.noreply.github.com>
2023-04-14 12:06:31 +0300

    Скрипт, реализующий 2 конкурирующих процесса
    по CPU

---
+++ comp_cpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+Pid="$(pgrep bash)"
+#taskset -p -c 3 $Pid
+
+nice -n 5 time gzip -c huge_file > huge5.gz &
+nice -n 20 time gzip -c huge_file > huge20.gz &
+Pid="$(pgrep gzip)"
+for proc in $Pid
+do
+taskset -p -c 3 $proc
+done
+
+ps fo pid,pcpu,pri,ni,cmd
+
+wait
